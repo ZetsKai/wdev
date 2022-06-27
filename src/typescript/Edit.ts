7c11bec5844496d7eed8e35a2509745f7eab69eb
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2022-06-27 02:02:42 -0400

    Create Edit.ts

---
+++ src/typescript/Edit.ts
@@ -0,0 +1,19 @@
+// import { ListTag } from "./ListTag";
+// import { PageTag } from "./PageTag";
+
+class Edit {
+	tag: JQuery<HTMLElement> = $(".sidebar--edit");
+
+	changeSelectedTag = (property: string, value: any) => $(".selected").css(property, value);
+	updateView = () => {
+		$(".tag-name").html($(".selected").children(".preview").html());
+		$(".colors__selected").css("background-color", "red");
+	}
+}
+
+export const EditingSidebar: Edit = new Edit();
+
+$(".colors__input").on("change", function(e) {
+	e.stopPropagation();
+	EditingSidebar.changeSelectedTag("background-color", $(this).val());
+});
