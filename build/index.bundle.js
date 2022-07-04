698df52f87d330b7e56f1794f1f0563a14a80c0e
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2022-07-04 09:35:58 -0400

    Update index.bundle.js

--- build/index.bundle.js
+++ build/index.bundle.js
@@ -1,85 +1,94 @@
 $(function () {
-class Edit {
-    tag = $(".sidebar--edit");
-    changeSelectedTag = (property, value) => $(".selected").css(property, value);
-    updateView = () => {
-        $(".tag-name").html($(".selected").children(".preview").html());
-        $(".colors__selected").css("background-color", $("#page-view > .selected").css("background-color"));
-    };
-}
-const EditingSidebar = new Edit();
-$(".colors__input").on("change", function (e) {
-    e.stopPropagation();
-    EditingSidebar.changeSelectedTag("background-color", $(this).val());
-    EditingSidebar.updateView();
-});
-
 class Tag {
     tag;
-    name;
-    constructor(tagType, classo) {
-        this.tag = $(`<${tagType} class="${classo}"></${tagType}>`);
-    }
-    setName(newName) {
-        this.name = newName;
-    }
-    select() {
-        this.tag.parent().children().removeClass("selected");
-        this.tag.addClass("selected");
-    }
-}
-
-class ListTag extends Tag {
-    PageTagClass;
-    constructor(tagName, PageTagClass) {
-        super(tagName, "sidebar--list__tag");
-        this.PageTagClass = PageTagClass;
-        this.newName(PageTagClass.name);
-    }
-    newName = (newName = `${this.tag[0].nodeName}#${this.tag.index(`#page-view ${this.tag[0].nodeName}`) + 1}`) => {
-        super.setName(newName);
-        this.tag.html(this.name);
-    };
-    handleSelect = () => {
-        super.select();
-        this.PageTagClass.select();
-    };
-}
-
-class PageTag extends Tag {
+    listTag;
     constructor(tagType) {
-        super(tagType, "tag-box");
-        this.tag.append(`<div class="blue preview"></div>`);
-        this.newName();
+        const nodeName = tagType.toLowerCase();
+        this.tag = $(`<${nodeName} class="tag-box"><div class="preview"></div></${nodeName}>`);
+        this.listTag = $(`<div class="blue sidebar--list__tag"></div>`);
     }
-    newName = (newName = `${this.tag[0].nodeName}#${this.tag.index(`#page-view > ${this.tag[0].nodeName}`) + 1}`) => {
-        super.setName(newName);
-        this.tag.children(".preview").html(this.name);
+    select = (event) => {
+        event.stopPropagation();
+        this.tag.addClass("selected");
+        this.listTag.addClass("selected");
+        return this;
+    };
+    addChild = (newChild) => this.tag.append(newChild.tag);
+    setName = (newName) => {
+        this.tag.children(".preview").html(newName);
+        this.listTag.html(newName);
     };
 }
 
-    function addNewTag(tagType) {
-        const newPageTag = new PageTag(tagType);
-        $("#page-view").append(newPageTag.tag);
-        newPageTag.newName();
-        const newListTag = new ListTag("div", newPageTag);
-        newListTag.tag.on("click", () => {
-            newListTag.handleSelect();
-            EditingSidebar.updateView();
-        });
-        $(".sidebar--list").append(newListTag.tag);
+class EditingSidebar {
+    selectedClass;
+    changeTag = (property, value) => {
+        if (this.selectedClass == undefined)
+            return;
+        this.selectedClass.tag.css(property, value);
+    };
+    updateView = () => {
+        let name;
+        let background;
+        if (this.selectedClass !== undefined) {
+            name = this.selectedClass.tag.children(".preview").html();
+            background = $("#page-view .selected").css("background-color");
+        }
+        else {
+            name = "None";
+            background = "transparent";
+        }
+        $(".tag-name").val(name);
+        $(".colors__selected").css("background-color", background);
+    };
+    setClass = (classo = undefined) => this.selectedClass = classo;
+}
+const Edit = new EditingSidebar();
+$(".colors__input").on("change", function (e) {
+    e.stopPropagation();
+    Edit.changeTag("background-color", $(this).val());
+    Edit.updateView();
+});
+$(".tag-name").on("keydown", function (e) {
+    if (e.key !== "Enter")
+        return;
+    const newName = $(this).val();
+    Edit.selectedClass?.setName(newName);
+});
+
+let selected;
+function deselect(e) {
+    e.stopPropagation();
+    if (selected == undefined)
+        return;
+    $(".selected").removeClass("selected");
+    selected = undefined;
+    Edit.setClass();
+    Edit.updateView();
+}
+    function addNewtag(tagType) {
+        const newTag = new Tag(tagType);
+        const nodeName = tagType.toLowerCase();
+        if (selected !== undefined) {
+            selected.addChild(newTag);
+            newTag.setName(`${nodeName}#${selected.tag.children(`${nodeName}.tag-box`).length}`);
+        }
+        else {
+            $("#page-view").append(newTag.tag);
+            newTag.setName(`${nodeName}#${$("#page-view").children(nodeName).length}`);
+        }
+        function handleSelect(e) {
+            deselect(e);
+            selected = newTag.select(e);
+            Edit.setClass(selected);
+            Edit.updateView();
+        }
+        $(".sidebar--list").append(newTag.listTag);
+        newTag.tag.children(".preview").on("click", handleSelect);
+        newTag.listTag.on("click", handleSelect);
     }
-    const setElementName = (tag, newName) => tag.children(".preview").html(newName);
-    function updateTags() {
-        const children = $("#page-view").children();
-        children.each(function () {
-            const elementName = this.nodeName;
-            const index = $(`#page-view ${elementName}`).index($(this)) + 1;
-            setElementName($(this), `${elementName}#${index}`);
-        });
-    }
-    $(".sidebar--tools__button").on("click", (e) => addNewTag(e.target.innerHTML));
-    $($(".top__button")[3]).on("click", updateTags);
-    $($(".top__button")[2]).on("click", EditingSidebar.updateView);
+    $(".sidebar--list").on("click", deselect);
+    $(".sidebar--tools__button").on("click", (e) => addNewtag(e.target.innerHTML));
+    $($(".top__button")[2]).on("click", () => $(".css-property"));
 });
 //# sourceMappingURL=index.bundle.js.map
